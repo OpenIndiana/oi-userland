49981c74711c4c133ee86098386e204fffd20bc0
Mike Sullivan <Mike.Sullivan@Oracle.COM>
2012-02-02 08:23:28 -0800

    Close of build 9.

--- make-rules/ips-buildinfo.mk
+++ make-rules/ips-buildinfo.mk
@@ -69,7 +69,7 @@ PLATNUM ?= 0
 # the development build) of the Solaris Update is being built.
 # This is set by the gatekeepers.
 #
-BUILDID ?= 8
+BUILDID ?= 9
 
 # Each (nightly) build of the code that produces packages needs to
 # be uniquely identified so that packages produced by different
