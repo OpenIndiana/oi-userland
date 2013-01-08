25f12bc47a22d59e143e2cef073dd73b246909a7
Mike Sullivan <Mike.Sullivan@Oracle.COM>
2013-01-07 21:58:19 -0800

    Close of build 13.

--- make-rules/ips-buildinfo.mk
+++ make-rules/ips-buildinfo.mk
@@ -18,7 +18,7 @@
 #
 # CDDL HEADER END
 #
-# Copyright (c) 2010, 2012, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2010, 2013, Oracle and/or its affiliates. All rights reserved.
 #
 
 
@@ -61,7 +61,7 @@ PLATNUM ?= 0
 # the development build) of the Solaris Update is being built.
 # This is set by the gatekeepers.
 #
-BUILDID ?= 12
+BUILDID ?= 13
 
 # Each (nightly) build of the code that produces packages needs to
 # be uniquely identified so that packages produced by different
