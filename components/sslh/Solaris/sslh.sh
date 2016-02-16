0c1010ebf51e74daba3752829b0f9a2a5e8c1211
benf33 <bentahyr@chez.com>
2016-02-15 18:48:39 -0800

    Add component sslh

---
+++ components/sslh/Solaris/sslh.sh
@@ -0,0 +1,53 @@
+#!/sbin/sh
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+
+#
+# Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
+#
+
+# Standard prolog
+#
+. /lib/svc/share/smf_include.sh
+
+if [ -z $SMF_FMRI ]; then
+        echo "SMF framework variables are not initialized."
+        exit $SMF_EXIT_ERR
+fi
+
+#
+# Is sslh configured?
+#
+if [ ! -f /etc/sslh.conf ]; then
+	echo "Error: Configuration file '/etc/sslh.conf' not found." \
+	    "  See sslh(8)."
+	exit $SMF_EXIT_ERR_CONFIG
+fi
+
+#
+# Build the command line flags
+#
+shift $#
+set -- -P /var/run/sslh.pid
+
+/usr/sbin/sslh -F /etc/sslh.cfg "$@"
+
+exit $SMF_EXIT_OK
