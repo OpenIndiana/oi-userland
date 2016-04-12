9949cb0d86eae3234623ee6867e41b5575b89f5b
Jim Klimov <jim@jimklimov.com>
2016-04-12 23:31:06 +0200

    tntnet-run-once.sh : quote SMF_FMRI, abort on non-smf runs, restart self in the end

--- components/library/tntnet/Solaris/tntnet-run-once.sh
+++ components/library/tntnet/Solaris/tntnet-run-once.sh
@@ -6,17 +6,19 @@
 # Inspired by https://docs.oracle.com/cd/E26502_01/html/E21383/smfdelivery.html
 . /lib/svc/share/smf_include.sh || exit $?
 
+[ -n "${SMF_FMRI}" ] || exit $SMF_EXIT_ERR_NOSMF
+
 if [ "$1" = "--refresh" ]; then
     echo "`LANG=C TZ=UTC date -u`: INFO: Self-enabling to check if the $0 script is updated"
-    svcadm enable $SMF_FMRI
-    svcadm clear $SMF_FMRI 2>/dev/null
+    svcadm enable "${SMF_FMRI}"
+    svcadm clear "${SMF_FMRI}" 2>/dev/null
 fi
 
-script_cksum=$(/usr/bin/svcprop -p config/script_cksum $SMF_FMRI)
+script_cksum=$(/usr/bin/svcprop -p config/script_cksum "${SMF_FMRI}")
 CKSUM="`cksum "$0" | awk '{print $1}'`"
 if [ "$script_cksum" == "$CKSUM" ] ; then
-    echo "`LANG=C TZ=UTC date -u`: INFO: Self-disabling, until the $0 script is updated and $SMF_FMRI is refreshed"
-    svcadm disable $SMF_FMRI
+    echo "`LANG=C TZ=UTC date -u`: INFO: Self-disabling, until the $0 script is updated and "${SMF_FMRI}" is refreshed"
+    svcadm disable "${SMF_FMRI}"
     exit $SMF_EXIT_OK
 fi
 
@@ -42,6 +44,7 @@ MKDIR() {
 MKDIR webservd:webservd 755 /var/www
 MKDIR webservd:webservd 755 /var/log/tntnet
 
-svccfg -s $SMF_FMRI setprop config/script_cksum = "$CKSUM" && \
-svccfg -s $SMF_FMRI refresh && \
-echo "`LANG=C TZ=UTC date -u`: INFO: Left a notice for my future self to quickly not-run, unless the $0 script is updated"
+svccfg -s "${SMF_FMRI}" setprop config/script_cksum = "$CKSUM" && \
+svccfg -s "${SMF_FMRI}" refresh && \
+echo "`LANG=C TZ=UTC date -u`: INFO: Left a notice for my future self to quickly not-run, unless the $0 script is updated" && \
+svcadm restart "${SMF_FMRI}"
