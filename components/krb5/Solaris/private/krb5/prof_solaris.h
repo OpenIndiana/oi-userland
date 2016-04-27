8affe1712e3fc71c9662642ecfb8c0decf0135b6
Neng Xue <neng.xue@oracle.com>
2016-04-27 16:11:34 -0700

    23109064 Userland MIT kerberos package should deliver lint libraries for several MIT libs

--- components/krb5/Solaris/private/krb5/prof_solaris.h
+++ components/krb5/Solaris/private/krb5/prof_solaris.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
  */
 
 /*
@@ -14,28 +14,28 @@
 #include <profile.h>
 #include <sys/types.h>
 
-#define K5_PROFILE_VAL_SUCCESS                  0
-#define K5_PROFILE_VAL_DEF_REALM_CASE           1
-#define K5_PROFILE_VAL_REALM_CASE               2
-#define K5_PROFILE_VAL_NO_DEF_IN_REALM          3
-#define K5_PROFILE_VAL_NO_DEF_REALM             4
-#define K5_PROFILE_VAL_NULL_REALM               5
-#define K5_PROFILE_VAL_NO_DOM_REALM_MAP         6
-#define K5_PROFILE_VAL_KDC_NO_REALM             7
-#define K5_PROFILE_VAL_ADMIN_NO_REALM           8
-#define K5_PROFILE_VAL_DOM_REALM_CASE           9
-#define K5_PROFILE_VAL_NO_REALM                 10
+#define	K5_PROFILE_VAL_SUCCESS					0
+#define	K5_PROFILE_VAL_DEF_REALM_CASE			1
+#define	K5_PROFILE_VAL_REALM_CASE				2
+#define	K5_PROFILE_VAL_NO_DEF_IN_REALM			3
+#define	K5_PROFILE_VAL_NO_DEF_REALM				4
+#define	K5_PROFILE_VAL_NULL_REALM				5
+#define	K5_PROFILE_VAL_NO_DOM_REALM_MAP			6
+#define	K5_PROFILE_VAL_KDC_NO_REALM				7
+#define	K5_PROFILE_VAL_ADMIN_NO_REALM			8
+#define	K5_PROFILE_VAL_DOM_REALM_CASE			9
+#define	K5_PROFILE_VAL_NO_REALM					10
 
 typedef struct _profile_options {
-       char *name;
-       int  *value;
-       int  found;
+		char *name;
+		int  *value;
+		int  found;
 } profile_options_boolean;
 
 typedef struct _profile_times {
-       char *name;
-       char **value;
-       int  found;
+		char *name;
+		char **value;
+		int  found;
 } profile_option_strings;
 
 errcode_t
