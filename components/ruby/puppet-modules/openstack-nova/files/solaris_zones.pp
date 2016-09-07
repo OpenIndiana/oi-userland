327150fc91b190d9a5e8644e055dbc65d08e045e
Laszlo Peter <laszlo.peter@oracle.com>
2016-09-07 14:48:35 -0700

    24454308 update all openstack puppet modules to the latest mitaka release

--- components/ruby/puppet-modules/openstack-nova/files/solaris_zones.pp
+++ components/ruby/puppet-modules/openstack-nova/files/solaris_zones.pp
@@ -7,7 +7,7 @@
 #
 # [*glancecache_dirname*]
 #   (optional) Default path to local image cache.
-#   Defaults to $state_path/images
+#   Defaults to /var/share/nova/images
 #
 # [*solariszones_snapshots_directory*]
 #   (optional) Location where solariszones driver will store instance
@@ -15,16 +15,16 @@
 #   Defaults to $instances_path/snapshots
 #
 class nova::compute::solaris_zones(
-    $glancecache_dirname='$state_path/images',
+    $glancecache_dirname='/var/share/nova/images',
     $solariszones_snapshots_directory='$instances_path/snapshots',
 ) {
 
   nova_config {
-    'DEFAULT/glancecache_dirname':   value => $glancecache_dirname;
-    'DEFAULT/solariszones_snapshots_directory':   value => $solariszones_snapshots_directory;
-    'DEFAULT/compute_driver':        value => 'solariszones.SolarisZonesDriver';
-    'DEFAULT/pybasedir':             value => '/usr/lib/python2.7/vendor-packages';
-    'DEFAULT/fping_path':            value => '/usr/bin/fping';
-    'DEFAULT/network_driver':        value => 'nova.network.solaris_net';
+    'DEFAULT/compute_driver':                          value => 'solariszones.SolarisZonesDriver';
+    'DEFAULT/pybasedir':                               value => '/usr/lib/python2.7/vendor-packages';
+    'DEFAULT/fping_path':                              value => '/usr/bin/fping';
+    'DEFAULT/network_driver':                          value => 'nova.network.solaris_net';
+    'solariszones/glancecache_dirname':                value => $glancecache_dirname;
+    'solariszones/solariszones_snapshots_directory':   value => $solariszones_snapshots_directory;
   }
 }
