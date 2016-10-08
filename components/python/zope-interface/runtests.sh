23d84283881e883bfb869dcb586fdcb0a029d9e5
Jingning Ji <jingning.ji@oracle.com>
2016-10-07 17:53:08 -0700

    23094049 Upgrade zope-interface to 4.1.3

--- components/python/zope-interface/runtests.sh
+++ components/python/zope-interface/runtests.sh
@@ -1,10 +1,15 @@
 #! /bin/sh
 
 for test in \
+    test_adapter \
     test_advice \
+    test_declarations \
     test_document \
     test_element \
+    test_exceptions \
     test_odd_declarations \
+    test_registry \
+    test_ro \
     test_sorting \
     test_verify \
     ; do
