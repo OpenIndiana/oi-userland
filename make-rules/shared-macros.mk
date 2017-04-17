a642e6c1c58b1b74c167d945bcf8a8481c35b082
Mike Sullivan <Mike.Sullivan@Oracle.COM>
2017-04-17 16:38:34 -0700

    25824193 userland should move to parfait 1.9.3

--- make-rules/shared-macros.mk
+++ make-rules/shared-macros.mk
@@ -480,7 +480,7 @@ BUILD_TOOLS ?=	/ws/on12-tools
 SPRO_ROOT ?=	$(BUILD_TOOLS)/SUNWspro
 SPRO_VROOT ?=	$(SPRO_ROOT)/solarisstudio12.4
 
-PARFAIT_ROOT =	$(BUILD_TOOLS)/parfait/parfait-tools-1.9.0
+PARFAIT_ROOT =	$(BUILD_TOOLS)/parfait/parfait-tools-1.9.3
 PARFAIT_TOOLS=	$(WS_TOOLS)/$(MACH)/parfait
 PARFAIT= $(PARFAIT_ROOT)/bin/parfait
 export PARFAIT_NATIVESUNCC=$(SPRO_VROOT)/bin/cc
