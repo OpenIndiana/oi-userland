5f3d5324c273248d26543905b25df94f96073f8c
Alexander Pyhalov <apyhalov@gmail.com>
2017-06-28 12:18:19 +0300

    nginx: update to 1.13.2, drop 32-bit version

--- components/web/nginx/files/nginx-startup.sh
+++ components/web/nginx/files/nginx-startup.sh
@@ -49,35 +49,8 @@ if [ "${PROPVAL}" != "" ] ; then
     NGINX_ETC_ROOT=$PROPVAL
 fi
 
-getprop httpd/enable_64bit
-if [ "${PROPVAL}" != "" ] ; then
-    case ${PROPVAL} in
-    true|1)
-        # Check if the system architecture supports 64-bit applications
-        PLATFORM=`isainfo -b`
-        if [ "${PLATFORM}" != "64" ]; then
-            echo "This system is not capable of supporting 64-bit applications."
-            echo "Set \"enable_64bit\" property value to \"false\" to start the 32-bit server."
-            exit $SMF_EXIT_ERR_CONFIG
-        fi
-
-	# 64 bit Apache
-	PLATFORM_DIR=/amd64
-	;;
-    false|0) 
-	# 32 bit Apache
-	PLATFORM_DIR=
-	;;
-    *)
-	# Invalid value for "bitness"
-	echo "\"bitness\" property value is invalid. Starting the server in 32-bit mode"
-	PLATFORM_DIR=
-	;;
-    esac
-fi
-
 NGINX_HOME=${NGINX_USR_ROOT}
-NGINX_BIN=${NGINX_HOME}/sbin${PLATFORM_DIR}
+NGINX_BIN=${NGINX_HOME}/sbin
 
 getprop httpd/startup_options
 if [ "${PROPVAL}" != "" ] ; then
