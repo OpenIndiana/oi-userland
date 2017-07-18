d8e615e61c4e4f081d35c1187e5e0a3ddff1f26f
Alexander Pyhalov <apyhalov@gmail.com>
2017-07-18 22:51:50 +0300

    pgtcl: update to 2.2.0, rebuild with tcl 8.6

--- components/tcl/pgtcl/files/pkgIndex.tcl
+++ components/tcl/pgtcl/files/pkgIndex.tcl
@@ -1,6 +1,8 @@
 global tcl_platform
 if {$tcl_platform(wordSize) == 8} {
-  package ifneeded Pgtcl 1.9  [list load [file join $dir amd64/libpgtcl1.9.so]]
+  package ifneeded Pgtcl 2.2  [list load [file join $dir amd64/libpgtcl2.2.0.so]]
 } else {
-  package ifneeded Pgtcl 1.9  [list load [file join $dir libpgtcl1.9.so]]
+  package ifneeded Pgtcl 2.2  [list load [file join $dir libpgtcl2.2.0.so]]
 }
+package ifneeded sc_postgres 1.2 \
+    [list source [file join $dir postgres-helpers.tcl]]
