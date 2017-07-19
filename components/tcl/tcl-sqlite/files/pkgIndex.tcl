983e7b3dc1512e288d8e90e471f33f7e35ff3196
Alexander Pyhalov <apyhalov@gmail.com>
2017-07-19 10:06:38 +0300

    tcl-sqlite: update to 3.19.3

--- components/tcl/tcl-sqlite/files/pkgIndex.tcl
+++ components/tcl/tcl-sqlite/files/pkgIndex.tcl
@@ -5,9 +5,9 @@
 #
 global tcl_platform
 if {$tcl_platform(wordSize) == 8} {
-  package ifneeded sqlite3 3.13.0 \
-    [list load [file join $dir amd64/libsqlite3.13.0.so] Sqlite3]
+  package ifneeded sqlite3 3.19.3 \
+    [list load [file join $dir amd64/libsqlite3.19.3.so] Sqlite3]
 } else {
-  package ifneeded sqlite3 3.13.0 \
-    [list load [file join $dir libsqlite3.13.0.so] Sqlite3]
+  package ifneeded sqlite3 3.19.3 \
+    [list load [file join $dir libsqlite3.19.3.so] Sqlite3]
 }
