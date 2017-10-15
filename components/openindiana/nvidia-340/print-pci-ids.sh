5706d98261e1043ecf0a8118dc0d120735d850cc
Andre Lupa <andre@cogeco.ca>
2017-10-14 21:01:12 -0400

    Fix wrong pci ids of nvidia drivers 304 and 384

--- components/openindiana/nvidia-340/print-pci-ids.sh
+++ components/openindiana/nvidia-340/print-pci-ids.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
+#
+# Pass the source directory and get a list of driver aliases in return.
 
-eval `grep ^A $1/NVDAgraphicsr/install/postinstall`
+eval "$(egrep '^A[1-9][0-9]*=|^ALIASES=' $1/NVDAgraphicsr/install/postinstall)"
 
 for i in $ALIASES; do
 	printf "\talias=$i \\\\\n"
 done
-
-
