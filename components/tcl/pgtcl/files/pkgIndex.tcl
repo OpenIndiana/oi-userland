88061cd54f968c45771959ca087d25b937b3f8cd
Alexander Pyhalov <apyhalov@gmail.com>
2019-11-22 13:26:13 +0300

    pgtcl: update to 2.6.3

--- components/tcl/pgtcl/files/pkgIndex.tcl
+++ components/tcl/pgtcl/files/pkgIndex.tcl
@@ -1,8 +1,8 @@
 global tcl_platform
 if {$tcl_platform(wordSize) == 8} {
-  package ifneeded Pgtcl 2.3.2  [list load [file join $dir amd64/libpgtcl2.3.2.so]]
+  package ifneeded Pgtcl 2.6.3  [list load [file join $dir amd64/libpgtcl2.6.3.so]]
 } else {
-  package ifneeded Pgtcl 2.3.2  [list load [file join $dir libpgtcl2.3.2.so]]
+  package ifneeded Pgtcl 2.6.3  [list load [file join $dir libpgtcl2.6.3.so]]
 }
-package ifneeded sc_postgres 1.2 \
+package ifneeded sc_postgres 1.3 \
     [list source [file join $dir postgres-helpers.tcl]]
