db24ad061bab07ac4bdc157e483b071854264b5d
Gary Mills <gary_mills@fastmail.fm>
2021-08-16 14:30:15 -0500

    New python module isort, version 5.9.3

---
+++ components/python/isort/OI/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='isort',
+    version='5.9.3',
+    description='A Python utility / library to sort imports',
+    url='https://github.com/PyCQA/isort',
+    packages=['isort'],
+)
