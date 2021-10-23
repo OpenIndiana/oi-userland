873dddf7ab0697f6440f5a3d1531c402d4145f6c
Gary Mills <gary_mills@fastmail.fm>
2021-10-23 14:11:45 -0500

    isort: include sub-modules

--- components/python/isort/OI/setup.py
+++ components/python/isort/OI/setup.py
@@ -5,5 +5,5 @@ setup(
     version='5.9.3',
     description='A Python utility / library to sort imports',
     url='https://github.com/PyCQA/isort',
-    packages=['isort'],
+    packages=['isort', 'isort._future', 'isort._vendored.toml', 'isort.deprecated', 'isort.stdlibs'],
 )
