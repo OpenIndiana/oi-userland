51d946fdcae13ee378526daeb7772db07dcef39b
Marcel Telka <marcel@telka.sk>
2023-02-25 23:16:43 +0100

    python/isort: update to 5.12.0

--- components/python/isort/OI/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='isort',
-    version='5.9.3',
-    description='A Python utility / library to sort imports',
-    url='https://github.com/PyCQA/isort',
-    packages=['isort', 'isort._future', 'isort._vendored.toml', 'isort.deprecated', 'isort.stdlibs'],
-)
