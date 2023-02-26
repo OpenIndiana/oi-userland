2f0605ea8381b96b018e51cea0dd889990c148e4
Marcel Telka <marcel@telka.sk>
2023-02-26 16:00:29 +0100

    setup.py.mk: TOX_TESTENV_PASSENV and --recreate are no longer needed for tox>=4 and recent tox-current-env

--- make-rules/setup.py.mk
+++ make-rules/setup.py.mk
@@ -302,9 +302,8 @@ ifeq ($(strip $(TEST_STYLE)),tox)
 COMPONENT_TEST_ENV +=		PATH=$(PATH)	# https://github.com/tox-dev/tox/issues/2538
 COMPONENT_TEST_ENV +=		PYTEST_ADDOPTS="$(PYTEST_ADDOPTS)"
 COMPONENT_TEST_ENV +=		NOSE_VERBOSE=2
-COMPONENT_TEST_ENV +=		TOX_TESTENV_PASSENV="PYTHONPATH PYTEST_ADDOPTS"
 COMPONENT_TEST_CMD =		$(TOX)
-COMPONENT_TEST_ARGS =		--current-env --no-provision --recreate
+COMPONENT_TEST_ARGS =		--current-env --no-provision
 COMPONENT_TEST_TARGETS =	-e py$(shell echo $(PYTHON_VERSION) | tr -d .)
 
 # Make sure following tools are called indirectly to properly support tox-current-env
