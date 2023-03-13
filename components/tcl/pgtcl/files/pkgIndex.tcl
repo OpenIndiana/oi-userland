7d0088d8a6fd94f3d187a8429420337d22d947fc
klausz65 <61782506+klausz65@users.noreply.github.com>
2023-03-13 19:18:20 +0100

    pgtcl: drop 32bit add SPARC vis instruction option

--- components/tcl/pgtcl/files/pkgIndex.tcl
+++
@@ -1,8 +0,0 @@
-global tcl_platform
-if {$tcl_platform(wordSize) == 8} {
-  package ifneeded Pgtcl 2.6.3  [list load [file join $dir amd64/libpgtcl2.6.3.so]]
-} else {
-  package ifneeded Pgtcl 2.6.3  [list load [file join $dir libpgtcl2.6.3.so]]
-}
-package ifneeded sc_postgres 1.3 \
-    [list source [file join $dir postgres-helpers.tcl]]
