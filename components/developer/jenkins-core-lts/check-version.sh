d902c22605f60c5a21a9797351dd9de3fef241f5
Jim Klimov <jim@jimklimov.com>
2023-05-30 11:55:27 +0200

    jenkins-core-*/check-version.sh: adapt to markup changes of https://get.jenkins.io/ directory listing to detect newest UPSTREAM_VERSION properly again

--- components/developer/jenkins-core-lts/check-version.sh
+++ components/developer/jenkins-core-lts/check-version.sh
@@ -2,14 +2,14 @@
 
 # Find the latest (tarball) release under original site
 #
-# Copyright 2016-2020 Jim Klimov
+# Copyright 2016-2023 Jim Klimov
 #
 
 BASE_URL="https://get.jenkins.io/war-stable/"
 DL_PAGE_URL="https://www.jenkins.io/download/"
 
 echo "=== Checking latest numbered release under $BASE_URL..."
-UPSTREAM_VERSION="`wget -q -O - "$BASE_URL" 2>/dev/null | egrep 'DIR.*[0-9]*\.[0-9]*' | grep -vw latest | tail -1 | sed 's,^.*a href="\([0-9]*\.[0-9]*\.[0-9]*\)/*".*,\1,'`" \
+UPSTREAM_VERSION="`wget -q -O - "$BASE_URL" 2>/dev/null | egrep 'href="[0-9]*\.[0-9]*\.[0-9]*/"' | grep -vw latest | sed 's,^.*a href="\([0-9]*\.[0-9]*\.[0-9]*\)/*".*,\1,' | sort -t. -k1n,1n -k2n,2n -k3n,3n | tail -1`" \
     && [ -n "$UPSTREAM_VERSION" ] && echo "Latest UPSTREAM_VERSION   = $UPSTREAM_VERSION" \
     || echo "WARNING: Could not fetch an UPSTREAM_VERSION, check manually at $DL_PAGE_URL " >&2
 
