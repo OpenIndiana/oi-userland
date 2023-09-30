c7825a95c897bf6d01e75233803d61cb92f40b5f
Marcel Telka <marcel@telka.sk>
2023-10-01 01:08:53 +0200

    shared-macros.mk: set default CLANG_VERSION to 17

--- make-rules/shared-macros.mk
+++ make-rules/shared-macros.mk
@@ -684,7 +684,7 @@ endif
 LD =		/usr/bin/ld
 
 # Clang definitions (we only have 64 bit clang)
-CLANG_VERSION           ?= 15
+CLANG_VERSION           ?= 17
 CLANG_FULL_VERSION      ?= $(CLANG_VERSION).0
 CLANG_PREFIX             = /usr/clang/$(CLANG_FULL_VERSION)
 CLANG_LIBDIR             = $(CLANG_PREFIX)/lib
