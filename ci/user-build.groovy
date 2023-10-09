332bc2281fe2c29dd660a404b160175a3a7319ad
Till Wegmueller <toasterson@gmail.com>
2023-10-09 12:01:55 +0200

    Ensure build does not fail at mounting as it may already be mounted
    Signed-off-by: Till Wegmueller <toasterson@gmail.com>

--- ci/user-build.groovy
+++ ci/user-build.groovy
@@ -8,7 +8,7 @@ pipeline {
     stages {
         stage('Ensure is ready to build') {
             steps {
-                sh 'pfexec /usr/sbin/mountall -F nfs'
+                sh 'pfexec /usr/sbin/mountall -F nfs || exit 0'
             }
         }
         stage('Git Checkout') {
