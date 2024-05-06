95037c185870a4c2d0adbff65bc1aac16b8728c2
Marcel Telka <marcel@telka.sk>
2024-05-06 19:23:07 +0200

    pyproject.mk: generate poetry test dependencies

--- make-rules/pyproject.mk
+++ make-rules/pyproject.mk
@@ -56,14 +56,22 @@ COMPONENT_POST_INSTALL_ACTION += \
 		$(MV) $(PROTO_DIR)/$(PYTHON_DIR)/site-packages $(PROTO_DIR)/$(PYTHON_LIB) ; \
 	fi ;
 
-# Generate raw lists of pdm test dependencies per Python version
+# Generate raw lists of poetry and pdm test dependencies per Python version
 COMPONENT_POST_INSTALL_ACTION += \
 	cd $(@D)$(COMPONENT_SUBDIR:%=/%) ; \
+	cfg=$(BUILD_DIR)/pyproject_deps-$(PYTHON_VERSION).json ; \
+	$(RM) $$cfg ; \
+	for p in $(TEST_REQUIREMENTS_POETRY) ; do \
+		$(PYTHON) -m pyproject_installer deps --depsconfig $$cfg add poetry_$$p poetry $$p ; \
+	done ; \
 	for p in $(TEST_REQUIREMENTS_PDM) ; do \
-		$(PYTHON) -m pyproject_installer deps --depsconfig $(BUILD_DIR)/pyproject_deps_pdm_$$p.json add $$p pdm $$p ; \
-		$(PYTHON) -m pyproject_installer deps --depsconfig $(BUILD_DIR)/pyproject_deps_pdm_$$p.json sync ; \
-		$(PYTHON) -m pyproject_installer deps --depsconfig $(BUILD_DIR)/pyproject_deps_pdm_$$p.json eval --depformat '$$nname' ; \
-	done >> $(@D)/.depend-test ;
+		$(PYTHON) -m pyproject_installer deps --depsconfig $$cfg add pdm_$$p pdm $$p ; \
+	done ; \
+	if [ -f $$cfg ] ; then \
+		$(PYTHON) -m pyproject_installer deps --depsconfig $$cfg sync ; \
+		$(PYTHON) -m pyproject_installer deps --depsconfig $$cfg eval --depformat '$$nname' \
+			>> $(@D)/.depend-test ; \
+	fi ;
 
 # Add build dependencies from project metadata to REQUIRED_PACKAGES
 REQUIRED_PACKAGES_RESOLVED += $(BUILD_DIR)/META.depend.res
