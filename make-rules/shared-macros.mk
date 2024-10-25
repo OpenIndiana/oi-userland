0a97e21fabe58f44cb0363bb2c3b343d012050d1
Marcel Telka <marcel@telka.sk>
2024-10-25 19:59:21 +0200

    icu: update to 76.1; keep ICU 75 libraries

--- make-rules/shared-macros.mk
+++ make-rules/shared-macros.mk
@@ -1072,7 +1072,7 @@ TCLSH.8.6.sparc.64 =	/usr/bin/sparcv9/tclsh8.6
 TCLSH =		$(TCLSH.$(TCL_VERSION).$(MACH).$(BITS))
 
 # ICU library
-ICU_VERSION =			75
+ICU_VERSION =			76
 ICU_LIBRARY_PKG =		library/icu-$(ICU_VERSION)
 REQUIRED_PACKAGES_SUBST +=	ICU_LIBRARY_PKG
 
