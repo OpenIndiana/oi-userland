7f7bd1ec46935f2f97f7451634512dec46744b65
klausz65 <61782506+klausz65@users.noreply.github.com>
2025-06-16 21:28:48 +0200

    libevent2: normalize includes / add SPARC support

--- components/library/libevent2/files/event-config.h
+++ components/library/libevent2/files/event-config.h
@@ -1,4 +1,4 @@
-#if defined(_LP64) || defined (__amd64)
+#if defined(_LP64) || defined(__amd64) || defined(__sparcv9)
 #include <event2/event-config-64.h>
 #else
 #include <event2/event-config-32.h>
