e408a9193f1fbef40f03bcc4e433b3567727b2c7
Neal Sidhwaney <nealsid@users.noreply.github.com>
2025-07-05 04:16:02 -0400

    Updates to Emacs package
    - Update README to remove out of date information
    - Update site-start.el to set variables to GNU variants of several utilities
    - Update .gitignore
    - Update duplicate package name in emacs script
    - Remove 01-find.patch because the variable is now set in site-start.el

--- components/editor/emacs/augment/source.el
+++ components/editor/emacs/augment/source.el
@@ -1,3 +1,7 @@
+(set-variable 'find-program "/usr/gnu/bin/find")
+(set-variable 'Man-sed-program "/usr/gnu/bin/sed")
+(set-variable 'insert-directory-program "/usr/gnu/bin/ls")
+
 ;;; Path to Emacs C Sources.
 (when (string-match (regexp-quote "COMPONENT_VERSION") emacs-version)
   (setq find-function-C-source-directory
