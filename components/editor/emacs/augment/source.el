0def7df0708838233dc1886b1b1e8c9d25b5ac6c
Neal Sidhwaney <nealsid@users.noreply.github.com>
2025-07-08 02:30:39 -0400

    Fix how variables are set in site startup file
    - Man-sed-program should be Man-sed-command
    - Use setopt instead of set-variable to take into account that they are variables declared with defcustom

--- components/editor/emacs/augment/source.el
+++ components/editor/emacs/augment/source.el
@@ -1,6 +1,7 @@
 (set-variable 'find-program "/usr/gnu/bin/find")
-(set-variable 'Man-sed-program "/usr/gnu/bin/sed")
-(set-variable 'insert-directory-program "/usr/gnu/bin/ls")
+
+(setopt Man-sed-command "/usr/gnu/bin/sed"
+        insert-directory-program "/usr/gnu/bin/ls")
 
 ;;; Path to Emacs C Sources.
 (when (string-match (regexp-quote "COMPONENT_VERSION") emacs-version)
